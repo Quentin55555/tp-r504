90f398b518d995fab8adda07fc46867c0f44e5cc
Quentin <leloup.quentin27@gmail.com>
2024-09-11 09:53:53 +0200

    Modif test.py

--- TP1/test.py
+++ TP1/test.py
@@ -1,6 +1,17 @@
 import pytest
 import fonctions as f
 
-def test_1( ) :
-		assert f.puissance(2,3) == 8
-		assert f.puissance(2,2) == 4
+def test_1():
+    # Tests avec des valeurs positives
+    assert f.puissance(2, 3) == 8
+    assert f.puissance(2, 2) == 4
+
+def test_2():
+    # Tests avec des valeurs négatives
+    assert f.puissance(-2, 3) == -8   # -2^3 = -8
+    assert f.puissance(-2, 2) == 4    # -2^2 = 4
+    assert f.puissance(2, -3) == 0.125 # 2^-3 = 1/8 = 0.125
+    assert f.puissance(-2, -2) == 0.25 # -2^-2 = 1/4 = 0.25
+
+
+
