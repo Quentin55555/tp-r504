877850559d4500e18d85d3131e2809f74e121b7c
Quentin <leloup.quentin27@gmail.com>
2024-09-11 09:29:12 +0200

    pooprihj

--- TP1/fonctions.py
+++ TP1/fonctions.py
@@ -1,11 +1,10 @@
 def puissance(a, b):
-    # Vérification que les deux arguments sont des entiers
-    if not isinstance(a, int) or not isinstance(b, int):
+    
+# Vérification que les deux arguments sont des entiers
+    if not type(a) is int or not type(b) is int:
         raise TypeError("Only integers are allowed")
 
     # Calcul de la puissance
     return a ** b
-
-
 
 
--- TP1/prog1.py
+++ TP1/prog1.py
@@ -1,5 +1,5 @@
 # prog1.py
-import fonctions as f
+import fonctions as f  # Importation du module contenant la fonction puissance
 
 # Saisie des valeurs de a et b par l'utilisateur
 a = input("Veuillez entrer la valeur (a) : ")
@@ -17,10 +17,10 @@ try:
     print(f"Le résultat de {a} à la puissance {b} est {res}.")
 
 except ValueError:
+    # Gestion de l'erreur si la conversion en entier échoue
     print("Erreur : Les valeurs doivent être des entiers.")
+
 except TypeError as e:
+    # Gestion de l'erreur si la fonction puissance lève une exception TypeError
     print(f"Erreur : {e}")
-
-
-
 
